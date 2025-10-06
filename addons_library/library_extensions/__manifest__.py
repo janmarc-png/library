4f4255d6ef70be04a29db52b923a498a9adb41b0
ja <janmarccolomaaa@gmail.com>
2025-10-06 12:46:05 +0000

    added access rights

--- addons_library/library_extensions/__manifest__.py
+++ addons_library/library_extensions/__manifest__.py
@@ -6,9 +6,10 @@
     'author': 'Jan Marc Coloma',
     'depends': ['library'],
     'data': [
-    'views/library_book_category_views.xml',
-    'views/library_menus.xml',
-    'views/library_book_views.xml',
+        'security/ir.model.access.csv',
+        'views/library_book_category_views.xml',
+        'views/library_menus.xml',
+        'views/library_book_views.xml',
     ],
     'installable': True,
     'application': False,
