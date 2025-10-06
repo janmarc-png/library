fe7e663134e7c5f7adcd906f20431bd20d9ad514
ja <janmarccolomaaa@gmail.com>
2025-10-06 12:36:20 +0000

    added author and categories field

---
+++ addons_library/library_extensions/models/library_book_category.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class LibraryBookCategory(models.Model):
+    _name = 'library.book.category'
+    _description = 'Book Category'
+    _order = 'name'
+
+    name = fields.Char(
+        string='Category Name',
+        required=True,
+        help='Name of the book category'
+    )
+
+    _sql_constraints = [
+        ('category_name_unique', 'unique(name)', 'Category name must be unique!'),
+    ]
